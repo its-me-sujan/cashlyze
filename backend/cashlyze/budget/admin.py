d2e7d146a9ec1cf6af0020693128b7c3421ca399
stha sujan <sthasujan1374@gmail.com>
2024-09-25 15:30:44 +0545

    register model in admin

--- backend/cashlyze/budget/admin.py
+++ backend/cashlyze/budget/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
+admin.site.register(Account)
+admin.site.register(Expense)
+admin.site.register(Income)
+admin.site.register(TransactionHistory)
 # Register your models here.
