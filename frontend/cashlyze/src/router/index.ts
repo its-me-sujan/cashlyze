cabca07884b6667808d09b3390c6b689d44a4530
pranav <pranavrajbhandari123@gmail.com>
2024-09-26 14:50:59 +0545

    s

---
+++ frontend/cashlyze/src/router/index.ts
@@ -0,0 +1,17 @@
+// // src/router/index.js
+
+// import { createRouter, createWebHistory } from "vue-router";
+// import Home from "../components/layout/";
+// import About from "../views/About.vue";
+
+// const routes = [
+//   { path: "/", name: "Home", component: Home },
+//   { path: "/about", name: "About", component: About },
+// ];
+
+// const router = createRouter({
+//   history: createWebHistory(),
+//   routes,
+// });
+
+// export default router;
