923591bcb58e40916ecb2bb535b3834ef5e4208e
Stha Sujan <itsmesujangit@gmail.com>
2024-09-26 11:04:54 +0545

    feat(url): added urls for income, account, transaction history

--- backend/cashlyze/budget/urls.py
+++ backend/cashlyze/budget/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path,include
-from .views import ExpenseViewset
+from .views import *
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'expenses', ExpenseViewset, basename='expense')
+router.register(r'incomes', IncomeViewset, basename='income')
+router.register(r'accounts', AccountViewset, basename='account')
 
 urlpatterns = [
     path('',include(router.urls)),
+    path('transaction-historys/', TransactionHistoryViewset.as_view(), name='transaction-history'), 
 ]
