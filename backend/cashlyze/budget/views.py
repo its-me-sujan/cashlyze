513418c7b66816890824ff63acb2633f3a112a6f
Stha Sujan <itsmesujangit@gmail.com>
2024-09-26 11:02:17 +0545

    feat(views): creating views for incomme, account, transactionhistory

--- backend/cashlyze/budget/views.py
+++ backend/cashlyze/budget/views.py
@@ -1,7 +1,19 @@
 from rest_framework import generics, viewsets
 from .models import Expense
-from .serializers import ExpenseSerializer
+from .serializers import *
 
 class ExpenseViewset(viewsets.ModelViewSet):
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
+    
+class IncomeViewset(viewsets.ModelViewSet):
+    queryset = Income.objects.all()
+    serializer_class = IncomeSerializer
+    
+class AccountViewset(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+class TransactionHistoryViewset(generics.ListAPIView):
+    queryset = TransactionHistory.objects.all()
+    serializer_class = TransactionHistorySerializer
