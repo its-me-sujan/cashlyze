590adbdba5d185b34bd8cb45ebc05763a042c5f2
stha sujan <itsmesujangit@gmail.com>
2024-09-27 15:20:02 +0545

    implement aliasing

--- frontend/cashlyze/vite.config.ts
+++ frontend/cashlyze/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "url";
 import { quasar } from "@quasar/vite-plugin";
 
 // https://vitejs.dev/config/
@@ -10,4 +11,10 @@ export default defineConfig({
       sassVariables: "src/styles/quasar-variables.sass",
     }),
   ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "vue-socials": "vue-socials/dist/vue-socials.es.js",
+    },
+  },
 });
