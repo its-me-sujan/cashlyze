c08c254c16a29f6b748567daf1938eed457b1c30
stha sujan <itsmesujangit@gmail.com>
2024-10-03 17:48:08 +0545

    removed the filter

--- frontend/cashlyze/src/components/HelloWorld.vue
+++ frontend/cashlyze/src/components/HelloWorld.vue
@@ -14,22 +14,13 @@
 
 <script setup lang="ts">
 import { useAccountStore } from "../stores/account";
-import { computed, onBeforeMount, ref } from "vue";
+import { computed, onBeforeMount } from "vue";
 
-const loading = ref(false);
 const AccountStore = useAccountStore();
 
 const accounts = computed(() => {
   return AccountStore.AccountList;
 });
-
-const filter = async (page: number = 1) => {
-  loading.value = true;
-  await AccountStore.getAccount({
-    page: page,
-  });
-  loading.value = false;
-};
 
 onBeforeMount(async () => {
   await AccountStore.getAccount({});
