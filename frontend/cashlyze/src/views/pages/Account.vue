fa7f61014e249d423544620cb1a8e163373bf4ae
stha sujan <itsmesujangit@gmail.com>
2024-10-16 17:18:48 +0545

    feat(account): checking some cards

--- frontend/cashlyze/src/views/pages/Account.vue
+++ frontend/cashlyze/src/views/pages/Account.vue
@@ -1,7 +1,45 @@
 <template>
-  <div>account</div>
+  <div class="w-[70%] mx-auto my-2">
+    <q-card class="q-pa-md">
+      <div
+        class="flex flex-row justify-between border border-black px-2"
+        @click="toggleChart"
+      >
+        <q-icon name="arrow_drop_down" size="24px" v-if="!showChart" />
+        <q-slide-transition>
+          <div v-show="showChart" class="mx-2 my-2">
+            <canvas
+              id="canvas_accounts_pie_chart"
+              style="display: block; box-sizing: border-box"
+              height="375"
+              width="500"
+            ></canvas>
+          </div>
+        </q-slide-transition>
+        <div
+          id="list_accounts_total_container"
+          class="flex items-start space-x-2 mr-2"
+        >
+          <span class="mr-2">Total:</span>
+          <span
+            id="list_accounts_total"
+            class="flex items-start min-w-[30px] text-green-600"
+            >$13,627.71</span
+          >
+        </div>
+      </div>
+    </q-card>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const showChart = ref(false);
+
+function toggleChart() {
+  showChart.value = !showChart.value;
+}
+</script>
 
 <style lang="scss" scoped></style>
