d2325e2303dbe609ffea4248b16e4e2e36916f05
stha sujan <itsmesujangit@gmail.com>
2024-10-22 17:14:41 +0545

    feat(jwt token): added authentication token

--- backend/cashlyze/budget/urls.py
+++ backend/cashlyze/budget/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path,include
 from .views import *
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 router = DefaultRouter()
 router.register(r'expenses', ExpenseViewset, basename='expense')
@@ -10,4 +12,7 @@ router.register(r'accounts', AccountViewset, basename='account')
 urlpatterns = [
     path('',include(router.urls)),
     path('transaction-historys/', TransactionHistoryViewset.as_view(), name='transaction-history'), 
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # /api/token/
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # /api/token/refresh/
+
 ]
